0d08c75fca34a42c82d1952e34929b2e0f6aa759
Cristiano_Nogueira <cristiano.nog.woody@gmail.com>
2025-03-09 22:46:48 -0300

    feat: criando controller advice

--- src/main/java/com/cristiano/picpayChallenge/infrastructure/controllers/ControllerAdvice.java
+++ src/main/java/com/cristiano/picpayChallenge/infrastructure/controllers/ControllerAdvice.java
@@ -1,6 +1,53 @@
 package com.cristiano.picpayChallenge.infrastructure.controllers;
 
-@org.springframework.web.bind.annotation.ControllerAdvice
+import com.cristiano.picpayChallenge.domain.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(DocumentAlreadyRegistered.class)
+    public ResponseEntity<String> documentAlreadyRegistered(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("Conflito: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(EmailAlreadyRegistered.class)
+    public ResponseEntity<String> emailAlreadyRegistered(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("Conflito: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<String> insufficientBalanceException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body("Erro: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(MerchantTransactionException.class)
+    public ResponseEntity<String> merchantTransactionException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body("Erro: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<String> resourceNotFound(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Erro: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(TransactionRefusedException.class)
+    public ResponseEntity<String> transactionRefusedException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body("Erro: " + ex.getMessage());
+    }
 }
--- src/main/java/com/cristiano/picpayChallenge/infrastructure/protocols/TransactionRepositoryImpl.java
+++ src/main/java/com/cristiano/picpayChallenge/infrastructure/protocols/TransactionRepositoryImpl.java
@@ -2,12 +2,15 @@ package com.cristiano.picpayChallenge.infrastructure.protocols;
 
 import com.cristiano.picpayChallenge.domain.entities.Transaction;
 import com.cristiano.picpayChallenge.implementation.Services.protocols.TransactionRepository;
+import com.cristiano.picpayChallenge.infrastructure.protocols.jpa.TransactionRepositoryJpa;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TransactionRepositoryImpl implements TransactionRepository {
 
     @Autowired
-    private TransactionRepository transactionRepository;
+    private TransactionRepositoryJpa transactionRepository;
 
     @Override
     public void save(Transaction transaction) {
